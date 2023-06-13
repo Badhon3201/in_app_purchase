472ae75da7db9e2aae8fd48b98cd8ae5ae29d348
Md. Akramuzzaman SIddique <49567193+Badhon3201@users.noreply.github.com>
2023-06-13 10:24:57 +0600

    Update in_app_item_id.dart
    app name changed

--- lib/in_app_purchase/in_app_item_id.dart
+++ lib/in_app_purchase/in_app_item_id.dart
@@ -1,5 +1,5 @@
 class InAppItemId{
-  static const String nonConsumableIdForAdRemove = "com.riseuplabs.islamicquote.removeads";
+  static const String nonConsumableIdForAdRemove = "com.riseuplabs.akram.removeads";
   static String kUpgradeId = 'upgrade';
   static String kSilverSubscriptionId = 'subscription_silver';
   static String kGoldSubscriptionId = 'subscription_gold';
